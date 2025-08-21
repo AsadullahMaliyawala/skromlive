314903eb2344e58ea6ed6205814ff3fb641021aa
Codebater <117357627+AsadullahMaliyawala@users.noreply.github.com>
2025-08-21 06:05:57 +0500

    Update route.ts

--- src/app/api/account/password/route.ts
+++ src/app/api/account/password/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/options'
 import { prisma } from '@/lib/prisma'
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcrypt'
 
 export async function PUT(req: Request) {
   const session = await getServerSession(authOptions as any)
